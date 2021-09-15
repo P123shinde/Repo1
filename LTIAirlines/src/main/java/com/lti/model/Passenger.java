c6e3549b0c49465f89e95f01e6a184d151df9200
P123shinde <shinde.pooja@walchandsangli.ac.in>
2021-09-15 13:16:32 +0530

    added passenger from br1

--- LTIAirlines/src/main/java/com/lti/model/Passenger.java
+++ LTIAirlines/src/main/java/com/lti/model/Passenger.java
@@ -1,5 +1,18 @@
 package com.lti.model;
 
 public class Passenger {
-
+ int passengerId;
+ String name;
+public int getPassengerId() {
+	return passengerId;
+}
+public void setPassengerId(int passengerId) {
+	this.passengerId = passengerId;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
 }
